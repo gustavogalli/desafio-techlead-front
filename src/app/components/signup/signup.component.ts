b7976c959b18e90f9c63e633358ca13ab49ea5d3
gustavogalli <gustavosgalli@gmail.com>
2023-05-28 18:48:33 -0300

    Setting SIGNUP component up

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { Customer } from '../../models/Customer';
+import { CustomerService } from 'src/app/services/customer.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -10,15 +14,45 @@ export class SignupComponent {
 
   customer: Customer = new Customer();
 
-  passwordCheck: string;
+  passwordCheck: string = ''
 
-  constructor() { }
+  constructor(
+    private service: CustomerService,
+    private toastr: ToastrService,
+    private router: Router
+  ) { }
+
+  name: FormControl = new FormControl(null, [Validators.minLength(3), Validators.required]);
+  cpf: FormControl = new FormControl(null, [Validators.required]);
+  email: FormControl = new FormControl(null, [Validators.email, Validators.required]);
+  password: FormControl = new FormControl(null, [Validators.minLength(5), Validators.required]);
 
   ngOnInit() { 
     window.scroll(0, 0)
   }
 
+  validateFiels(): boolean {
+    return this.name.valid && this.cpf.valid && this.email.valid && this.password.valid;
+  }
+
   signup(){
+    console.log("customer preenchido")
+    console.log(this.customer)
+    this.service.create(this.customer).subscribe(() => {
+      alert("ok")
+    //   this.toastr.success('User created!', 'New user', { timeOut: 4000 });
+    //   this.customer = new Customer();
+    //   this.router.navigate(['/login'])
+    // }, exception => {
+    //   if(exception.error.errors){
+    //     exception.error.errors.forEach(element => {
+    //       this.toastr.error(element.message);
+    //     });
+    //   } else {
+    //     this.toastr.error(exception.error.message);
+    //   }
+    // 
+  })
   }
 
 }
--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Customer } from '../models/Customer';
+import { API_CONFIG } from '../config/api.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<Customer[]>{
+    return this.http.get<Customer[]>(`${API_CONFIG.baseUrl}/customers`)
+  }
+
+  findById(id: number): Observable<Customer>{
+    return this.http.get<Customer>(`${API_CONFIG.baseUrl}/customers/${id}`);
+  }
+
+  create(customer: Customer): Observable<Customer>{
+    return this.http.post<Customer>(`${API_CONFIG.baseUrl}/customers`, customer);
+  }
+
+  update(customer: Customer): Observable<Customer>{
+    return this.http.put<Customer>(`${API_CONFIG.baseUrl}/customers/${customer.id}`, customer);
+  }
+
+  delete(id: number): Observable<void>{
+    return this.http.delete<void>(`${API_CONFIG.baseUrl}/customers/${id}`);
+  }
+
+  
 }
