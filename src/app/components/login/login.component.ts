4e1762e7e44d50a0e80d28a4596ff2983d3e5b56
gustavogalli <gustavosgalli@gmail.com>
2023-05-28 10:27:00 -0300

    Login component created

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0, 0)
+  }
+
+  login() {
+ 
+  }
+
+}
