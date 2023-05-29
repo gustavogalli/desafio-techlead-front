0d9f58b0fd17c15d439390829671f1812024f54d
gustavogalli <gustavosgalli@gmail.com>
2023-05-29 12:51:43 -0300

    Bringing BOOK and CUSTOMER to LOAN-MANAGEMENT

--- src/app/components/loan-management/loan-management.component.ts
+++ src/app/components/loan-management/loan-management.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Book } from 'src/app/models/Book';
+import { Customer } from 'src/app/models/Customer';
+import { Loan } from 'src/app/models/Loan';
+import { BookService } from 'src/app/services/book.service';
+import { CustomerService } from 'src/app/services/customer.service';
+import { LoanService } from 'src/app/services/loan.service';
 
 @Component({
   selector: 'app-loan-management',
@@ -6,5 +12,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./loan-management.component.css']
 })
 export class LoanManagementComponent {
+
+  books: Book[];
+  customers: Customer[];
+  loans: Loan[];
+
+  constructor(
+    private service: LoanService,
+    private bookService: BookService,
+    private customerService: CustomerService
+  ){}
+
+  ngOnInit(){
+    this.findAllLoans();
+  }
+
+  findAllLoans(){
+    this.service.findAll().subscribe(response => {
+      this.loans = response;
+    })
+  }
 
 }
