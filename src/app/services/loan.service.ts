db3a5677a92d1d07aa323704b75c11c806589b6c
gustavogalli <gustavosgalli@gmail.com>
2023-05-29 12:32:26 -0300

    Adding CRUD to LOAN service

--- src/app/services/loan.service.ts
+++ src/app/services/loan.service.ts
@@ -11,7 +11,23 @@ export class LoanService {
 
   constructor(private http: HttpClient) { }
 
+  findAll(): Observable<Loan[]>{
+    return this.http.get<Loan[]>(`${API_CONFIG.baseUrl}/loan`)
+  }
+
+  findById(id: number): Observable<Loan>{
+    return this.http.get<Loan>(`${API_CONFIG.baseUrl}/loan/${id}`);
+  }
+
   create(loan: Loan): Observable<Loan>{
     return this.http.post<Loan>(`${API_CONFIG.baseUrl}/loan`, loan);
+  }
+
+  update(loan: Loan): Observable<Loan>{
+    return this.http.put<Loan>(`${API_CONFIG.baseUrl}/loan/${loan.id}`, loan);
+  }
+
+  delete(id: number): Observable<void>{
+    return this.http.delete<void>(`${API_CONFIG.baseUrl}/loan/${id}`);
   }
 }
