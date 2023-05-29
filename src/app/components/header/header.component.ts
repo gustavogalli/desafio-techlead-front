6cdf918066976e1b37d577e8d1655e08896fc01b
gustavogalli <gustavosgalli@gmail.com>
2023-05-29 00:28:11 -0300

    HEADER edited

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  constructor(
+    private router: Router, 
+    private authService: AuthService,
+    private toastr: ToastrService
+    ){
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  logout(){
+    this.router.navigate(['/login']);
+    this.authService.lougout();
+    this.toastr.info('You have successfully logged out!', null, { timeOut: 7000 })
+  }
 
 }
